1ae840073d246f44f94a282994fece5c603e9fb0
Luisa <lu.andra.808@gmail.com>
2024-07-13 10:20:29 -0500

    ejercicios de python

---
+++ print.py
@@ -0,0 +1,3 @@
+print("Me encanta estudiar python")
+print("Estudiar con Python Total es super divertido")
+print(600-45)
---
+++ strings.py
@@ -0,0 +1,10 @@
+# print('Línea 1\nLínea 2\nLínea 3')
+# print('A\tB\tC \nD\tE\tF \nG\tH\tI')
+
+# input("Dime tu nombre: ")
+'''print("Estas estudiando: " + input("¿Qué estás estudiando? "))
+print("Vives en: " + input("¿En qué país vives? "))
+print("Tu nombre completo es: " + input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: ")  )'''
+
+print("El nombre de la cerveza es: " + input("Ayuda a generar el nombre de una cerveza respondiendo dos preguntas \n1.¿Qué palabra se viene a tu cabeza cuando piensas en cerveza? ") + " " + input("2.l¿Qué emoción te genera la cerveza? "))
+
