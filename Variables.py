a264008bf95e4f20d3520639a4eeda74dcffbe65
Luisa <lu.andra.808@gmail.com>
2024-07-13 13:10:42 -0500

    mas ejemplos

---
+++ Variables.py
@@ -0,0 +1,29 @@
+# nombre="Tony Soprano"
+# edad= 51
+# print(nombre , edad)
+# nombre= "Julia"
+# apellido="Roberts"
+# nombrecompleto= nombre + " " + apellido
+
+# print(nombrecompleto)
+
+# curso="Python"
+# print("Esta tomando un curso de " + curso)
+
+# numero=1.5
+# print(type(numero))
+
+# num_entero=5
+# print(type(num_entero))
+
+# num_decimal=5.5
+# print(type(num_decimal))
+
+# num1=7.5
+# num2=2.5
+# suma= num1+num2
+# print(type(suma))
+
+x=10
+y=5
+print("Mis numeros son {} y {}" .format(x,y))
